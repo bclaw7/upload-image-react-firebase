3204dfc56a97853d7a79a21fc1bedaa9e85099c2
bclaw <bclaw8300@gmail.com>
2021-06-24 14:51:14 -0600

    added upload button

---
+++ src/firebase/index.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+import "firebase/storage";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyApEpJPtsxhIx6QEnbJrnLqlw07BNEurdY",
+	authDomain: "fir-react-upload-b2f25.firebaseapp.com",
+	projectId: "fir-react-upload-b2f25",
+	storageBucket: "fir-react-upload-b2f25.appspot.com",
+	messagingSenderId: "1084361617349",
+	appId: "1:1084361617349:web:6c5b4c07e47450b7cce5ac",
+	measurementId: "G-6MZ5H02QNF",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const storage = firebase.storage();
+
+export { storage, firebase as default };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import { render } from "react-dom";
+import { storage } from "./firebase";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const ReactFirebaseFileUpload = () => {
+	const [image, setImage] = useState(null);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const [url, setUrl] = useState('');
+
+	const handleChange = (e) => {
+		if (e.target.files[0]) {
+			setImage(e.target.files[0]);
+		}
+	};
+
+	const handleUpload = () => {
+		const uploadTask = storage.ref(`images/${image.name}`).put(image);
+		uploadTask.on(
+			"state-changed",
+			(snapshot) => {},
+			(error) => {
+				console.log(error);
+			},
+			() => {
+				storage
+					.ref("images")
+					.child(image.name)
+					.getDownloadURL()
+					.then(url => {
+						setUrl(url);
+					});
+			}
+		);
+	};
+
+	console.log("image: ", image);
+
+	return (
+		<div>
+            <br />
+			<input type="file" onChange={handleChange} />
+			<button onClick={handleUpload}>Upload</button>
+            <br />
+            {url}
+		</div>
+	);
+};
+
+render(<ReactFirebaseFileUpload />, document.querySelector("#root"));
